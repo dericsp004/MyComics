daf20fbc1734ff55c291b94d4ad10809575219f0
Deric Plummer <dericsp004@gmail.com>
2017-08-15 07:01:18 -0700

    created collapsable comics list-experimented with jquery and css

--- MyComics/wwwroot/js/site.js
+++ MyComics/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿// Write your Javascript code.
+﻿// site.js
+(function () {
+    var comicListWrapper = $("#comicListWrapper");
+    $("#hideComicsButton").on("click", function () {
+        comicListWrapper.toggleClass("hide");
+        if (comicListWrapper.hasClass("hide")) {
+            $(this).text("Show Comics")
+        }
+    });
+
+    
+})();
