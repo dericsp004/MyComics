03a8892bab11eb7ddb1fa6e4c2bdc87eb2a078ef
Deric Plummer <dericsp004@gmail.com>
2017-08-22 11:18:58 -0700

    stylized Favorites hide/show (Index)

--- MyComics/wwwroot/js/site.js
+++ MyComics/wwwroot/js/site.js
@@ -1,13 +1,18 @@
 ﻿// site.js
 (function () {
     var comicListWrapper = $("#comicListWrapper");
+    var icon = $("#hideComicsButton i");
     $("#hideComicsButton").on("click", function () {
         comicListWrapper.toggleClass("hideComics");
         if (comicListWrapper.hasClass("hideComics")) {
-            $(this).text("Show Comics")
+            //$(this).text("Show")
+            icon.removeClass("fa fa-angle-up");
+            icon.addClass("fa fa-angle-down");
         }
         else {
-            $(this).text("Hide Comics")
+            //$(this).text("Hide")
+            icon.removeClass("fa fa-angle-down");
+            icon.addClass("fa fa-angle-up");
         }
     });
 
